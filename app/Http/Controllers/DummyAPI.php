74f51b3a8a7f9cfe51879914046733837bf2114f
ALinks <accommodationlinks.co.uk@gmail.com>
2021-10-26 18:43:25 +0500

    searchAPI

--- app/Http/Controllers/DummyAPI.php
+++ app/Http/Controllers/DummyAPI.php
@@ -7,36 +7,45 @@ use App\Models\Member;
 
 class DummyAPI extends Controller
 {
-    function getData($id=null){
+    function getData($id = null)
+    {
         $member = new Member();
         return $id ? $member::find($id) : $member::all();
     }
 
-    function postData(Request $req){
+    function postData(Request $req)
+    {
         $member = new Member();
         $member->name = $req->name;
         $member->email = $req->email;
         $member->address = $req->address;
         $result = $member->save();
-        
-        return $result ? ["result"=>"Success"]
-        : ["result"=>"Failed"];
+
+        return $result ? ["result" => "Success"]
+            : ["result" => "Failed"];
     }
 
-    function putData(Request $req){
+    function putData(Request $req)
+    {
         $member = Member::find($req->id);
         $member->name = $req->name;
         $member->email = $req->email;
         $member->address = $req->address;
         $result = $member->save();
-        return $result ? ["result"=>"Success"]
-        : ["result"=>"Failed"];
+        return $result ? ["result" => "Success"]
+            : ["result" => "Failed"];
     }
 
-    function deleteData(Request $req){
-        $member = Member::find($req->id);
+    function deleteData(Request $req)
+    {
+        $member = Member::find($req->id)->get;
         $result = $member->delete();
-        return $result ? ["result"=>"Success, ".$req->id." has been deleted.!"]
-        : ["result"=>"Failed"];
+        return $result ? ["result" => "Success, " . $req->id . " has been deleted.!"]
+            : ["result" => "Failed"];
+    }
+
+    function searchData($name)
+    {
+        return Member::where('name','like','%'.$name.'%')->get();
     }
 }
